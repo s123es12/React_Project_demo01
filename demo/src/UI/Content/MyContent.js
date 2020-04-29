d701ddf3df7c2885e481720967490c69efcc8edc
Jerry <s123es12@gmail.com>
2020-04-29 22:42:57 +0800

    project v1.2.1, register 75% finished, UI 60% temp, menu dropdown UI 60%

--- demo/src/UI/Content/MyContent.js
+++ demo/src/UI/Content/MyContent.js
@@ -7,21 +7,64 @@ import {
     Icon,
     Input,
     Select,
-    Button
+    DatePicker,
+    Dropdown,
+    Button,
+    Calendar
 } from 'antd';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
 
 import "./content.less";
 import happydoctor from '../../images/happydoctor.jpg'
 import home2 from '../../images/home2.jpg'
 import online_shopping from '../../images/online-shopping.jpg'
 
+moment.locale('en');
 const { Option } = Select;
-
+const {RangePicker} = DatePicker
 class MyContent extends Component {
-    
+    state={
+        value:moment(),
+        dropdownMenuDataSelect:false
+    }
+    handleShowDropSelect=()=>{
+        let bool = !this.state.dropdownMenuDataSelect;
+        this.setState({dropdownMenuDataSelect:bool});
+    }
     render() { 
+        function onPanelChange(value, mode) {
+            console.log("onPanelChange(): ",value, mode);
+        }
         
-          
+        
+        const menu = (
+            <Menu>
+              <Menu.Item key="0">
+                <a href="http://www.alipay.com/">1st menu item</a>
+              </Menu.Item>
+              <Menu.Item key="1">
+                <a href="http://www.taobao.com/">2nd menu item</a>
+              </Menu.Item>
+              <Menu.Divider />
+              <Menu.Item key="3">3rd menu item</Menu.Item>
+            </Menu>
+          );
+          const menu2 = (
+             
+            <div style={{backgroundColor: "white",width: 300, border: '1px solid #d9d9d9', borderRadius: 4 }}>
+                <Calendar 
+                    
+                    fullscreen={false} 
+                    value={moment()} 
+                    onSelect={(data)=>console.log("onSelect(): "+data)} 
+                    onPanelChange={this.onPanelChange}
+                />
+            </div>   
+            
+            
+          );
+        
         return (
           <div className="content" style={{height:"100%",backgroundColor:"white"}}>
             <div className="content_menu">
@@ -46,20 +89,29 @@ class MyContent extends Component {
                  
                         
                         <div className="menu_inputDiv">
-                            <label htmlFor="input02">
-                                <div style={{padding:"10px"}}>
+                            <Dropdown overlay={menu2} trigger={['click']}
+                                visible={this.state.dropdownMenuDataSelect}
+                            >
+                                <Button 
+                                    onClick={e => {e.preventDefault();this.handleShowDropSelect();}}
+                                    style={{width:"100%",height:"100%",margin:"0"}}
+                                >
+                                     
+                                
                                     <h5>入住／退房</h5>
-                                    <Input id="input02" placeholder="新增日期" />
-                                </div>
-                            </label>
+                                       
+                                </Button>   
+                            </Dropdown>
                         </div>
-                        <div className="menu_inputDiv"style={{borderRight:"unset"}}>
-                            <label htmlFor="input03">
-                                <div style={{padding:"10px"}}>
-                                    <h5>旅人</h5>
-                                    <Input id="input03" placeholder="新增旅人" />
-                                </div>
-                            </label>
+                        <div className="menu_inputDiv" style={{borderRight:"unset"}}>
+                            <Dropdown overlay={menu} trigger={['click']}>
+                                <Button 
+                                    onClick={e => e.preventDefault()}
+                                    style={{width:"100%",height:"100%",margin:"0"}}
+                                ><h5 >旅人</h5>
+                                <input placeholder="wewewew" disabled="disabled"/>
+                                </Button>
+                            </Dropdown>
                         </div>
                         
                         <button icon="search" size="large"><Icon type="search"/>搜尋</button>
